f013837fcfdb9a3b0b8be2bb549af7e15352dc46
Lunatic-Neon <sj060700@naver.com>
2025-09-26 11:12:51 +0900

    4주차

---
+++ ch04/sample01.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+covid_file_path = './data/owid-covid-data.csv'
+# covid_file_path = './data/data_bar.csv'
+df = pd.read_csv(covid_file_path, sep='|', endocing='euc-kr')
+# print(df)
+print(type(df))
+
+
